d2a2da816c0f9d42ec41e203eefa5d41c74cc852
Mehmetbaran Geylani <66281850+brngylni@users.noreply.github.com>
2023-06-18 00:02:22 +0300

    database models have been added

---
+++ HotelHub.Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelHub.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        [Required]
+        public int Count { get; set; }
+    }
+}
