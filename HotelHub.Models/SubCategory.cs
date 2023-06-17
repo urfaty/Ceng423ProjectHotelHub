b6c82fb4675bf57b59afa13239bfa5035e051679
urfaty <97344694+urfaty@users.noreply.github.com>
2023-06-18 00:07:32 +0300

    Add files via upload
    Database Models Added

---
+++ HotelHub.Models/SubCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelHub.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]  
+        public string Title { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ICollection<Item> Items { get; set; }
+    }
+}
