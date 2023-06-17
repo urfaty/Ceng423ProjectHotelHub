1201875f0f198c6f6d61408c3069095073da82ee
Bpasazade <51914223+Bpasazade@users.noreply.github.com>
2023-06-18 00:07:29 +0300

    Database models added

---
+++ HotelHub.Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HotelHub.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [JsonIgnore]
+        [IgnoreDataMember]
+        public ICollection<Item> Items { get; set; }
+        [JsonIgnore]
+        [IgnoreDataMember]
+        public ICollection<SubCategory> SubCategories { get; set; }
+    }
+}
